b7d044a81ea513ee664ed65b30e9db7ae1ca7091
Csaba Fülöp <csfulop@users.noreply.github.com>
2019-08-18 20:19:09 +0200

    script to inference with ResNet

---
+++ resnet.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Inference with ResNet model
+
+Based on:
+    https://www.kaggle.com/dansbecker/tensorflow-programming
+    https://keras.io/applications/#usage-examples-for-image-classification-models
+
+Usage:
+    resnet.py <image path 1> ...
+"""
+
+#######################################
+
+import numpy as np
+from tensorflow.python.keras.applications.resnet50 import preprocess_input
+from tensorflow.python.keras.preprocessing.image import load_img, img_to_array
+
+image_size = 224
+
+def read_and_prep_images(img_paths, img_height=image_size, img_width=image_size):
+    imgs = [load_img(img_path, target_size=(img_height, img_width)) for img_path in img_paths]
+    img_array = np.array([img_to_array(img) for img in imgs])
+    output = preprocess_input(img_array)
+    return(output)
+
+#######################################
+
+from tensorflow.python.keras.applications import ResNet50
+import sys
+
+my_model = ResNet50(weights='imagenet')
+img_paths = sys.argv[1:]
+test_data = read_and_prep_images(img_paths)
+preds = my_model.predict(test_data)
+
+#######################################
+
+from keras.applications.resnet50 import decode_predictions
+import pprint
+print('Predicted:') 
+pprint.pprint(
+    list(zip(
+        img_paths,
+        decode_predictions(preds, top=3)
+    ))
+)
