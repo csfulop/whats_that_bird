e0035348d1a5d071763a220cf27cab603cc8b5a3
Csaba Fülöp <csfulop@users.noreply.github.com>
2019-08-19 15:21:53 +0200

    get number of classes from DirectoryIterator

--- transfer_learning_resnet.py
+++ transfer_learning_resnet.py
@@ -6,11 +6,32 @@ Based on:
     https://www.kaggle.com/dansbecker/transfer-learning
 """
 
+from tensorflow.python.keras.applications.resnet50 import preprocess_input
+from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
+
+image_size = 224
+data_generator = ImageDataGenerator(preprocessing_function=preprocess_input)
+
+train_generator = data_generator.flow_from_directory(
+    './bird_photos/train',
+    target_size=(image_size, image_size),
+    batch_size=20,
+    class_mode='categorical')
+
+validation_generator = data_generator.flow_from_directory(
+    './bird_photos/test',
+    target_size=(image_size, image_size),
+    class_mode='categorical')
+
+assert train_generator.num_classes == validation_generator.num_classes, 'training and validation classes must match'
+
+#######################################
+
 from tensorflow.python.keras.applications import ResNet50
 from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras.layers import Dense, Flatten, GlobalAveragePooling2D
+from tensorflow.python.keras.layers import Dense
 
-num_classes = 5
+num_classes = train_generator.num_classes
 
 bird_model = Sequential()
 bird_model.add(ResNet50(include_top=False, pooling='avg', weights='imagenet'))
@@ -25,27 +46,9 @@ bird_model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['
 
 #######################################
 
-from tensorflow.python.keras.applications.resnet50 import preprocess_input
-from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
-
-image_size = 224
-data_generator = ImageDataGenerator(preprocessing_function=preprocess_input)
-
-
-train_generator = data_generator.flow_from_directory(
-        './bird_photos/train',
-        target_size=(image_size, image_size),
-        batch_size=20,
-        class_mode='categorical')
-
-validation_generator = data_generator.flow_from_directory(
-        './bird_photos/test',
-        target_size=(image_size, image_size),
-        class_mode='categorical')
-
 bird_model.fit_generator(
-        train_generator,
-        steps_per_epoch=10,
-        epochs=10,
-        validation_data=validation_generator,
-        validation_steps=1)
+    train_generator,
+    steps_per_epoch=10,
+    epochs=10,
+    validation_data=validation_generator,
+    validation_steps=1)
