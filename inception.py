65663ae84fe83f2274b3206073e767233f0c613a
Csaba Fülöp <csfulop@users.noreply.github.com>
2019-08-25 19:34:12 +0200

    inference with original inception v3 model

--- inception.py
+++ inception.py
@@ -1,43 +1,42 @@
 """
-Inference with ResNet model
-
-Based on:
-    https://www.kaggle.com/dansbecker/tensorflow-programming
-    https://keras.io/applications/#usage-examples-for-image-classification-models
+Inference with Inception model
 
 Usage:
-    python resnet.py <image>...
+    python inception.py <image>...
 """
 
 #######################################
 
 import numpy as np
-from tensorflow.python.keras.applications.resnet50 import preprocess_input
+from keras.applications.inception_v3 import preprocess_input
 from tensorflow.python.keras.preprocessing.image import load_img, img_to_array
 
-image_size = 224
+image_size = 299
+
 
 def read_and_prep_images(img_paths, img_height=image_size, img_width=image_size):
     imgs = [load_img(img_path, target_size=(img_height, img_width)) for img_path in img_paths]
     img_array = np.array([img_to_array(img) for img in imgs])
     output = preprocess_input(img_array)
-    return(output)
+    return (output)
+
 
 #######################################
 
-from tensorflow.python.keras.applications import ResNet50
 import sys
+from keras.applications import InceptionV3
 
-my_model = ResNet50(weights='imagenet')
+my_model = InceptionV3(weights='imagenet')
 img_paths = sys.argv[1:]
 test_data = read_and_prep_images(img_paths)
 preds = my_model.predict(test_data)
 
 #######################################
 
-from keras.applications.resnet50 import decode_predictions
 import pprint
-print('Predicted:') 
+from keras.applications.inception_v3 import decode_predictions
+
+print('Predicted:')
 pprint.pprint(
     list(zip(
         img_paths,
